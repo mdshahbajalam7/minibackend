9419abbb6c50610ba854943a13d684513849cebc
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2022-12-23 22:58:54 +0530

    backend update

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
+const express = require("express")
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 const jsonServer = require("json-server");
+const https = require("https");
+const { appendFile } = require("fs");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
+const app = express()
+
+app.get("/fetch-user", async (req, res) => {
+    https
+      .get("https://randomuser.me/api", (result) => {
+        let data = "";
+        result.on("data", (chunk) => {
+          data += chunk;
+        });
+        result.on("end", () => {
+          data = JSON.parse(data);
+          data = data.results[0];
+          //   console.log(data);
+          res.send(data);
+        });
+      })
+      .on("error", (err) => {
+        console.log(err.message);
+      });
+  });
+  
+
+
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use("/api", router);
